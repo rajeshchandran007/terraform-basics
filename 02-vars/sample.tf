99ba47c845cd85db38681b885bf7c603beb601dc
Rajeshe <rajeshchandran007@gmail.com>
2022-12-04 09:23:56 +0530

    script changest

---
+++ 02-vars/sample.tf
@@ -0,0 +1,59 @@
+variable "sample" {
+  default  = "Hello World"   # Declaring the variable
+}
+
+output "sample" {
+    value = ${var.sample}       # Printing the variable
+}
+
+
+output "sample1" {
+    value = "The sample variable's value that I am Printing is ${var.sample}"
+}
+
+variable "number" {
+    default = 100 
+}
+
+output "number" {
+    value = "The number is: ${var.number}"       # Printing the variable
+}
+
+
+# List Variable 
+
+variable "ex-list" {
+  default = [
+    "Cloud",
+    "DevOps",
+    100,
+    false,
+    "Verma",
+    "SRE"
+  ]
+}
+
+output "ex-list-output" {
+    value = "Welcome to ${var.ex-list[0]} ${var.ex-list[1]} Training and the total course duration is ${var.ex-list[2]} and the trainer name is ${var.ex-list[4]}"
+}
+
+# Map Variable 
+
+variable "ex-map" {
+    default = {
+        Duration = 105 
+        Timing   = "0730AMIST"
+        BATCH    = 51 
+        
+    }
+  
+}
+
+output "ex-map-output" {
+    value = "ex-map-output: ${var.ex-map[Duration]} ${var.ex-map[Timing]}"
+}
+
+
+
+# A variable can be accessed without ${} only the variable is called. In case, if it's needed to be added with other strings or in between something, then we need to enclose them in
+# in the double quotes. Single quotes are not valid in terraform. 
