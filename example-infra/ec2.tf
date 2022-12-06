42f744066c864049634c4e348010f27619f25877
Rajeshe <rajeshchandran007@gmail.com>
2022-12-06 08:03:54 +0530

    script changest

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -14,36 +14,36 @@ output "private_dns" {
     value   =   aws_instance.app.private_dns
 }
 
-# Creates Security Group
-# resource "aws_security_group" "allow_ssh" {
-#   name        = "allow_ssh"
-#   description = "Allow SSH inbound traffic"
+Creates Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
 
-#   ingress {
-#     description      = "SSH from VPC"
-#     from_port        = 22
-#     to_port          = 22
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-#   ingress {
-#     description      = "HTTP from Public"
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
+  ingress {
+    description      = "HTTP from Public"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#     ipv6_cidr_blocks = ["::/0"]
-#   }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
-#   tags = {
-#     Name = "allow_SSH"
-#   }
-# }
+  tags = {
+    Name = "allow_SSH"
+  }
+}
