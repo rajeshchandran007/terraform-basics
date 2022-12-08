a829f3460daac47a55db2279e05d033609eea6c1
Rajeshe <rajeshchandran007@gmail.com>
2022-12-08 21:02:43 +0530

    script changest

--- example-modules-infra/info.tf
+++ example-modules-infra/info.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "rc-tf-remote-state-bucket"
-    key    = "sample/terraform.tfstate"
+    key    = "example2/terraform.tfstate"
     region = "us-east-1"
   }
 }
